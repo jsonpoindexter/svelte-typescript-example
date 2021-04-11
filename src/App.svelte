2fea30a72cd1872acefe4ff3c69b83716074f478
Jason Poindexter <poindexter.json@gmail.com>
2021-04-11 15:32:18 -0700

    initial commit

---
+++ src/App.svelte
@@ -0,0 +1,25 @@
+<svelte:head>
+	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/5.0.0-alpha2/css/bootstrap.min.css" integrity="sha384-DhY6onE6f3zzKbjUPRc2hOzGAdEf4/Dz+WJwBvEYL/lkkIsI3ihufq9hk9K4lVoK" crossorigin="anonymous">
+</svelte:head>
+
+<script lang="ts">
+   import Dashboard from './View/Dashboard.svelte' 
+   import Login from './View/Login.svelte'
+    export let name: string;
+    import { isAuthenticated } from './store.ts';
+</script>
+
+<main>
+    <div class="container bg-danger">
+        <h1>Hello {name}!</h1>
+        { #if $isAuthenticated }
+            <Dashboard />
+        {:else}
+            <Login />
+        {/if}
+        
+    </div>
+</main>
+
+<style>
+</style>
